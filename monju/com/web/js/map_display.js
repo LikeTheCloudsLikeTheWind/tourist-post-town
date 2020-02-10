6839bf5544a573271b202f1b3174bbb87e3159aa
takeshi nishio <takeshi.the.clouds.tech.acc@gmail.com>
2020-02-10 16:21:03 +0900

    ルート検索実装

--- monju/com/web/js/map_display.js
+++ monju/com/web/js/map_display.js
@@ -1,7 +1,29 @@
-var map = L.map('map').setView([36.575,135.984], 5);    // 日本を中心に設定
+var map = L.map('map').setView([35.449410, 137.404914], 15);    // 恵那文化センターを中心に設定　35.449410, 137.404914
 
+//スケールコントロールを最大幅200px、右下、m単位で地図に追加
+L.control.scale({ maxWidth: 200, position: 'bottomright', imperial: false }).addTo(map);
+
+//ズームコントロールを左下で地図に追加
+// L.control.zoom({ position: 'bottomleft' }).addTo(map);
+
+// OpenStreetMapをタイルとして使用
 var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
       attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
       maxZoom: 19
     });
 tileLayer.addTo(map);
+
+// マーカー設置
+L.marker([35.449410, 137.404914])
+ .addTo(map)    
+ .bindPopup('恵那文化センター')
+ .openPopup();; // 恵那文化センターの緯度,経度を指定してマーカーを立てる
+
+// ルーティング
+L.Routing.control({
+    waypoints: [
+        L.latLng(35.449410, 137.404914),
+        L.latLng(35.449447, 137.412828)
+    ],
+    routeWhileDragging: true
+}).addTo(map);
